68ef6db625adc051d2f0d20b4df28e9b6dbff27d
Fatih Canbekli <fatih.canbekli@outlook.com>
2019-06-14 03:15:33 +0300

    Project Init

---
+++ Strahd-Timer/Strahd-Timer/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  Strahd-Timer
+//
+//  Created by Fatih Canbekli on 14.06.2019.
+//  Copyright © 2019 Fatih Canbekli. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+        // Insert code here to initialize your application
+       
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+    
+}
+
---
+++ Strahd-Timer/Strahd-Timer/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Strahd-Timer
+//
+//  Created by Fatih Canbekli on 14.06.2019.
+//  Copyright © 2019 Fatih Canbekli. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+    @IBAction func StartButton(_ sender: NSButton) {
+        print("Test")
+    }
+    
+}
+
